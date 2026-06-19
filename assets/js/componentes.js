ed412b463c7e58ef262589bc7da025733959697f
Leonardo <leopires1988@gmail.com>
2026-06-19 14:02:27 -0300

    ajuste no botão zap

--- assets/js/componentes.js
+++ assets/js/componentes.js
@@ -97,6 +97,7 @@ async function loadComponent(containerId, componentPath) {
             setupMegaMenuTabs();
             setupMobileMenu();
             setupHeaderScroll();
+            setupWhatsappMessages();
         }
 
         if (containerId === 'footer-container') {
@@ -337,3 +338,51 @@ document.addEventListener('DOMContentLoaded', () => {
     loadComponent('header-container', 'componentes/header.html');
     loadComponent('footer-container', 'componentes/footer.html');
 });
+
+const messages = [
+    'Solicite sua cotação',
+    'Mudanças internacionais',
+    'Atendimento em mais de 150 países'
+];
+
+let current = 0;
+
+const msgs = [
+    'Solicite sua cotação',
+    'Mudança Internacional',
+    'Fale com um especialista'
+];
+
+let i = 0;
+const text = document.querySelector('.whatsapp-message');
+
+function setupWhatsappMessages() {
+    const text = document.querySelector('.whatsapp-message');
+
+    if (!text) return;
+
+    const msgs = [
+        '📦 Solicite sua cotação!',
+        '🌎 Mudanças Nacionais e Internacionais.',
+        '💬 Fale com um especialista agora!'
+    ];
+
+    let i = 0;
+
+    setInterval(() => {
+
+    text.style.opacity = 0;
+    text.style.transform = 'translateY(8px)';
+
+    setTimeout(() => {
+
+        i = (i + 1) % msgs.length;
+        text.textContent = msgs[i];
+
+        text.style.opacity = 1;
+        text.style.transform = 'translateY(0)';
+
+    }, 500);
+
+}, 5500);
+}
